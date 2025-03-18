7833ffa4a4fa57d60b0f26d6155027ef069b8f60
Axel Meunier <143808857+LeskGit@users.noreply.github.com>
2025-03-18 12:21:02 +0100

    mauvais clear

--- src/views/PageCharacters.js
+++ src/views/PageCharacters.js
@@ -8,7 +8,7 @@ export class PageCharacters extends InterfaceAffichage {
     }
 
     afficher() {
-        const container = document.getElementById("container");
+        const container = document.getElementById("view-container");
         container.innerHTML = "";
         for (const character of this.listCharacter) {
             const h3 = document.createElement("h3");
